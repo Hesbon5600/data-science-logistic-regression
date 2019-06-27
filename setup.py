dd54fcfa9f28f28f8094447471af5fa17e8bead6
Hesbon5600 <hesbonkiptoo5600@gmail.com>
2019-06-27 19:19:01 +0300

    data science basics
    - accuracy
    - binary predictors
    - clustering

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='make a much more fundamental forecast with logistiic regression',
+    author='Hesbon',
+    license='MIT',
+)
